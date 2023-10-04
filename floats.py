643bdbfd57ab72bc5cd1452e98f97bdc65deffc3
ephilbin <143443264+ephilbin@users.noreply.github.com>
2023-10-04 14:20:38 -0400

    Add files via upload

---
+++ floats.py
@@ -0,0 +1,12 @@
+# Calculate the product of three numbers
+#FIND 3 BUGS
+#Get user input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+#Calculate the product
+product = num1 * num2 * num3
+
+# Print the result
+print("The product is: " + str(product))
